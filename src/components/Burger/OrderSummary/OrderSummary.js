07ef1885d630c34c9f0b7bc88c4998d23b77f9e7
darthbel <febelgine@gmail.com>
2019-10-18 21:16:06 -0400

    minor optimizations

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,39 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import Button from '../../UI/Button/Button'
 
-const OrderSummary = props => {
-  const ingredientsSummary = Object.keys(props.ingredients).map(ingKey => {
-    return (
-      <li key={ingKey}>
-        <span style={{ textTransform: 'capitalize' }}>{ingKey}</span>:{' '}
-        {props.ingredients[ingKey]}
-      </li>
-    )
-  })
+class OrderSummary extends Component {
+  componentWillUpdate() {}
 
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientsSummary}</ul>
-      <p>
-        <strong>Total Price: ${props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType={'Danger'} clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType={'Success'} clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Fragment>
-  )
+  render() {
+    const ingredientsSummary = Object.keys(this.props.ingredients).map(
+      ingKey => {
+        return (
+          <li key={ingKey}>
+            <span style={{ textTransform: 'capitalize' }}>{ingKey}</span>:{' '}
+            {this.props.ingredients[ingKey]}
+          </li>
+        )
+      }
+    )
+
+    return (
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientsSummary}</ul>
+        <p>
+          <strong>Total Price: ${this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType={'Danger'} clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType={'Success'} clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Fragment>
+    )
+  }
 }
 
 export default OrderSummary
